b68cee7141b09d64039ac636add186220df1d981
승하 <@oshnew-home>
2016-12-18 11:07:56 +0900

    프로젝트 셋팅내용 커밋

---
+++ src/main/java/com/biz/SvcMonitoringApplication.java
@@ -0,0 +1,12 @@
+package com.biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvcMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvcMonitoringApplication.class, args);
+	}
+}
